f439d03195096e9e2cdc60244f483688a575c243
Ehimare Okoyomon <eokoyomon@berkeley.edu>
2016-12-27 01:14:32 -0500

    lol changed the table name from flares to scores... woops

--- migrations/20161222061138_scores.js
+++ migrations/20161222061138_scores.js
@@ -1,6 +1,6 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('flares', function (table) {
+  return knex.schema.createTable('scores', function (table) {
     table.string('user').notNullable();
     table.integer('score').notNullable();
     table.string('country').notNullable();
